f90145a2fa71cad9018b297b4c6e9e0c7de49de6
cwood89 <cwood89@mac.com>
2020-05-28 03:23:41 -0400

    finished tour of heroes

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Black Panther' },
+  { id: 12, name: 'Luke Cage' },
+  { id: 13, name: 'Green Lantern' },
+  { id: 14, name: 'Lucas Bishop' },
+  { id: 15, name: 'Steel' },
+  { id: 16, name: 'Blade' },
+  { id: 17, name: 'Cyborg' },
+  { id: 18, name: 'Spawn' },
+  { id: 19, name: 'Storm' },
+  { id: 20, name: 'Black Lightning' }
+];
