48fa4a00775b3132eff15c4aac368d4741c7af14
cwood89 <cwood89@mac.com>
2020-05-28 00:38:28 -0400

    added list of peeps

--- angularLab/README.md
+++ angularLab/README.md
@@ -1,4 +1,4 @@
-# AngularPractice
+# AngularLab
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- angularLab/e2e/src/app.e2e-spec.ts
+++ angularLab/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angularPractice app is running!');
+    expect(page.getTitleText()).toEqual('angularLab app is running!');
   });
 
   afterEach(async () => {
